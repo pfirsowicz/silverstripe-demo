64b7f0490d0d7dee01aad22a52367be18c72cd66
Pawel Firsowicz <pawel@xebidy.com>
2017-06-11 20:47:43 +1200

    Database config

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,8 +3,16 @@
 global $project;
 $project = 'mysite';
 
-global $database;
-$database = '';
+global $databaseConfig;
+
+$databaseConfig = array(
+        "type" => 'MySQLDatabase',
+        "server" => 'db685846262.db.1and1.com',
+        "username" => 'dbo685846262',
+        "password" => 'Silverstripe-demo1',
+        "database" => 'db685846262',
+        "path" => ''
+    );
 
 require_once 'conf/ConfigureFromEnv.php';
 
